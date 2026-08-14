51f8560cd5d909549063e1ca05a747bd6e4b53c3
labon8055 <ruhuloffice10@gmail.com>
2026-08-14 22:14:06 +0600

    New loop added

--- main.dart
+++ main.dart
@@ -1,8 +1,17 @@
 import 'mobile.dart';
-
+import 'dart:io';
 void main(){
   // ClassName variableName = ClassName();
   Mobile iPhone13 = Mobile(m: 'iPhone 13 Pro max', year: '2020', price: 123434.0);
   iPhone13.capturePhoto();
   iPhone13.captureVideo();
+
+  print('What is your name');
+  String? name = '';
+  name = stdin.readLineSync();
+  print('My name is $name');
+
+  for (var i = 1; i <=10; i++){
+    print('I love you ruhi');
+  }
 }
